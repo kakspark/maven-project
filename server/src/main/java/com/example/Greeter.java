8103ff3d6c35abe1b53ddade91830f1b86530095
Anil Kanaparthi <Anil_Kanaparthi@epam.com>
2019-03-28 12:36:43 +0530

    checkstyle warnings fix1

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,7 +16,8 @@ public class Greeter {
   }
   
   /**
-   * This is a class.
+   * @param someone the name of person
+   * @return greeting string
    */ 
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
